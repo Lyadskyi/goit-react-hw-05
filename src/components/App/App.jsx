8e05703b9d1d32ad2b487ff70953bb7f713b93f7
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-05-09 00:07:27 +0300

    Added API_KEY & API_TOKEN

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import HomePage from "../../pages/HomePage/HomePage";
 import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 import css from "./App.module.css";
 
@@ -13,6 +14,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}></Route>
           <Route path="*" element={<NotFoundPage />}></Route>
         </Routes>
       </Layout>
