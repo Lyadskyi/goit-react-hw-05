6ff4f13f1d007fa70cf81eac84a24a522b7d99c3
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-05-12 13:30:58 +0300

    Added asynchronous loading js-code using React.lazy & Suspense

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,32 @@
-import { Routes, Route } from "react-router-dom";
+import { lazy } from "react";
 import Layout from "../Layout/Layout";
-import HomePage from "../../pages/HomePage/HomePage";
-import MoviesPage from "../../pages/MoviesPage/MoviesPage";
-// import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
-// import MovieCast from "../MovieCast/MovieCast";
-// import MovieReviews from "../MovieReviews/MovieReviews";
-
+import { Routes, Route } from "react-router-dom";
 import css from "./App.module.css";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("../../pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage")
+);
 
 export default function App() {
   return (
-    <>
-      <Layout>
-        <h1 className={css.heading}>Trending today</h1>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          {/* <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route> */}
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Layout>
-    </>
+    <Layout>
+      <h1 className={css.heading}>Trending today</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
   );
 }
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,14 @@
 import Navigation from "../Navigation/Navigation";
+import { Suspense } from "react";
 import css from "./Layout.module.css";
 
 export default function Layout({ children }) {
   return (
     <div className={css.container}>
       <Navigation />
-      {children}
+      <Suspense fallback={<div>Please wait loading page...</div>}>
+        {children}
+      </Suspense>
     </div>
   );
 }
